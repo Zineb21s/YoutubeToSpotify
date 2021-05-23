91468485a08172d967f1e5788fc1b1803d7a52a8
Zineb21S <z.attaoui@aui.ma>
2021-05-23 15:51:37 +0100

    fix: fixed the infinite scroll, retrieving more than 100 songs

--- youtube_to_spotify.py
+++ youtube_to_spotify.py
@@ -1,29 +1,52 @@
-"""This script is to create a Spotify Playlist from a Youtube Playlist (up to 100 songs so far)."""
-import re
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.common.keys import Keys
 import sys
-import spotipy
-import requests
 from spotipy import util
-from bs4 import BeautifulSoup
+import spotipy
 import config
+import re
+from selenium.webdriver.chrome.options import Options
 
 
-def get_yt_songs(url):
-    """Returns all the titles from the Youtube Playlist's videos"""
-    # Due to infinite scroll, you can only get 100 songs with bs4, to be fixed.
-    source_code = requests.get(url).text
-    soup = BeautifulSoup(source_code, 'html.parser')
-    domain = 'https://www.youtube.com'
-    song_titles = []
-    for link in soup.find_all("a", {"dir": "ltr"}):
-        href = link.get('href')
-        if href.startswith('/watch?'):
-            # print(link.string.strip())
-            youtube_url = domain + href + '\n'
-            print(youtube_url)
-            song_titles.append(link.string.strip())
-    return song_titles
 
+def get_yt_songs(baseURL):
+    options = Options()
+    options.add_argument("window-size=1920x1080")
+    options.headless = True
+    videos = []
+    titles = []
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options= options)
+
+    driver.get(baseURL)
+
+    while True:
+        scroll_height = 2000
+        document_height_before = driver.execute_script("return document.documentElement.scrollHeight")
+        driver.execute_script(f"window.scrollTo(0, {document_height_before  + scroll_height});")
+        time.sleep(1.5)
+        document_height_after = driver.execute_script("return document.documentElement.scrollHeight")
+        if document_height_after == document_height_before:
+            break
+
+    videos= driver.find_elements_by_xpath('//*[@id="contents"]')
+
+
+    for i in range(0,len(videos)):
+        title = videos[i].find_elements_by_xpath('//*[@id="video-title"]')
+
+
+    print(len(title))
+
+
+
+    for i in range(0,len(title)):
+        titles.append(title[i].text)
+
+    print("Done...")
+    driver.quit()
+    return titles
 
 def clean_title(title):
     """Removes anything within (), [], ** and ft/feat in order to
@@ -34,7 +57,6 @@ def clean_title(title):
     title = title.lower().replace('feat', '')
     title = title.lower().replace('ft', '')
     return title
-
 
 def find_song(title):
     """Returns the song."""
@@ -53,39 +75,35 @@ def add_song(track, playlist, user):
     """Adds the song to the Spotify Playlist."""
     sp.user_playlist_add_tracks(user, playlist, [track])
 
-if len(sys.argv) > 1:
-    USERNAME = sys.argv[1]
-else:
-    print("Please enter username")
-    sys.exit()
 
-token = util.prompt_for_user_token(USERNAME, config.SCOPE, client_id=config.CLIENT_ID,
-                                   client_secret=config.CLIENT_SECRET, redirect_uri=config.REDIRECT)
+def youtube_to_spotify(USERNAME, URL):
+    global sp
+    token = util.prompt_for_user_token(USERNAME, config.SCOPE, client_id=config.CLIENT_ID,
+                                    client_secret=config.CLIENT_SECRET, redirect_uri=config.REDIRECT)
 
-if token:
-    sp = spotipy.Spotify(auth=token)
+    if token:
+        sp = spotipy.Spotify(auth=token)
 
-    # The youtube playlist must be public
-    URL = "https://www.youtube.com/playlist?list=PL_8qg6E2ChVQyMAn53OQkij73ESJWmRTn"
+        songs = get_yt_songs(URL)
+        TITLE = "Testing"
+        DESC = " "
+        sp.user_playlist_create(USERNAME, TITLE, public=True, description=DESC)
+        playlists = sp.current_user_playlists()
+        playlist_Id = playlists['items'][0]['id']
 
-    songs = get_yt_songs(URL)
-    TITLE = " "
-    DESC = " "
-    sp.user_playlist_create(USERNAME, TITLE, public=True, description=DESC)
-    playlists = sp.current_user_playlists()
-    playlist_Id = playlists['items'][0]['id']
-    for song in songs:
-        cleaned_title = clean_title(song)
-        if len(cleaned_title) > 0:
-            music = find_song(cleaned_title)
-            songId = get_song_id(music)
-            if songId:
-                add_song(songId, playlist_Id, USERNAME)
-                # print("yes?")
+        for song in songs:
+            cleaned_title = clean_title(song)
+            if len(cleaned_title) > 0:
+                music = find_song(cleaned_title)
+                songId = get_song_id(music)
+                if songId:
+                    add_song(songId, playlist_Id, USERNAME)
+                else:
+                    print(song + " not found!")
             else:
-                print(song + " not found!")
-        else:
-            print("The video has been deleted or it is private.")
+                print("The video has been deleted or it is private.")
 
-else:
-    print("nopeeee")
+    else:
+        print("nopeeee")
+
+youtube_to_spotify(	"username", "playlist_urt")
