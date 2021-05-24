acd1762e8db522732a07c99f785ceb4b54f7636d
Zineb21S <z.attaoui@aui.ma>
2021-05-24 18:46:02 +0100

    fix: quick fix of the oauth2

--- youtube_to_spotify.py
+++ youtube_to_spotify.py
@@ -3,13 +3,34 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.common.keys import Keys
 import sys
-from spotipy import util
+from spotipy import util, oauth2
 import spotipy
 import config
 import re
 from selenium.webdriver.chrome.options import Options
+import logging
 
+logger = logging.getLogger(__name__)
 
+def new_get_auth_response(self):
+    logger.info('User authentication requires interaction with your '
+                'web browser. Once you enter your credentials and '
+                'give authorization, you will be redirected to '
+                'a url.  Paste that url you were directed to to '
+                'complete the authorization.')
+
+    redirect_info = urlparse(self.redirect_uri)
+    redirect_host, redirect_port = util.get_host_port(redirect_info.netloc)
+
+    if redirect_port:
+        return self._get_auth_response_local_server(redirect_port)
+    else:
+        logger.warning('Using `%s` as redirect URI without a port. '
+                           'Specify a port (e.g. `%s:8080`) to allow '
+                           'automatic retrieval of authentication code '
+                           'instead of having to copy and paste '
+                           'the URL your browser is redirected to.',
+                           redirect_host, redirect_host)
 
 def get_yt_songs(baseURL):
     options = Options()
@@ -37,7 +58,7 @@ def get_yt_songs(baseURL):
         title = videos[i].find_elements_by_xpath('//*[@id="video-title"]')
 
 
-    print(len(title))
+    #print(len(title))
 
 
 
@@ -77,33 +98,38 @@ def add_song(track, playlist, user):
 
 
 def youtube_to_spotify(USERNAME, URL):
-    global sp
-    token = util.prompt_for_user_token(USERNAME, config.SCOPE, client_id=config.CLIENT_ID,
-                                    client_secret=config.CLIENT_SECRET, redirect_uri=config.REDIRECT)
+    try:
+        oauth2.get_auth_response = new_get_auth_response
+        global sp
+        token = util.prompt_for_user_token(USERNAME, config.SCOPE, client_id=config.CLIENT_ID,
+                                        client_secret=config.CLIENT_SECRET, redirect_uri=config.REDIRECT)
 
-    if token:
-        sp = spotipy.Spotify(auth=token)
+        if token:
+            sp = spotipy.Spotify(auth=token)
+            sp.trace = False
 
-        songs = get_yt_songs(URL)
-        TITLE = "Testing"
-        DESC = " "
-        sp.user_playlist_create(USERNAME, TITLE, public=True, description=DESC)
-        playlists = sp.current_user_playlists()
-        playlist_Id = playlists['items'][0]['id']
+            songs = get_yt_songs(URL)
+            TITLE = "Testing21"
+            DESC = " "
+            sp.user_playlist_create(USERNAME, TITLE, public=True, description=DESC)
+            playlists = sp.current_user_playlists()
+            playlist_Id = playlists['items'][0]['id']
 
-        for song in songs:
-            cleaned_title = clean_title(song)
-            if len(cleaned_title) > 0:
-                music = find_song(cleaned_title)
-                songId = get_song_id(music)
-                if songId:
-                    add_song(songId, playlist_Id, USERNAME)
+            for song in songs:
+                cleaned_title = clean_title(song)
+                if len(cleaned_title) > 0:
+                    music = find_song(cleaned_title)
+                    songId = get_song_id(music)
+                    if songId:
+                        add_song(songId, playlist_Id, USERNAME)
+                    else:
+                        print(song + " not found!")
                 else:
-                    print(song + " not found!")
-            else:
-                print("The video has been deleted or it is private.")
+                    print("The video has been deleted or it is private.")
 
-    else:
-        print("nopeeee")
+        else:
+            print("nopeeee")
+    except spotipy.SpotifyException:
+        print("Spotipy exception")
 
-youtube_to_spotify(	"username", "playlist_urt")
+youtube_to_spotify(	"username", "playlist_url")
